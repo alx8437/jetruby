72b9331242607efc7fd14c04c66c6fd6775f0efa
alx8437 <dezion777@gmail.com>
2021-02-11 19:28:03 +0300

    write sort func, double and render littleSquare

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import './App.css';
 import {v1} from "uuid";
 
 type Square = {
-  id: string,
-  color: string,
-  hidden: boolean,
+    id: string,
+    color: string,
+    hidden: boolean,
 }
 //
 // type StateType = Array<Square>
@@ -13,26 +13,35 @@ type Square = {
 
 const App = () => {
 
-  const [colors, setColors] = useState<Array<string>>(
-      ['red', 'orange', 'yellow', 'green', 'aquamarine', 'blue', 'magenta', 'indigo'])
+    const colors: Array<string> = [
+        'red', 'orange', 'yellow', 'green', 'aquamarine', 'blue', 'magenta', 'indigo',
+    ]
 
 
+    const sortArray = (array: Array<string>) => {
+        array.forEach((color, i) => {
+            const j = Math.floor(Math.random() * i)
+            const temp = array[i]
+            array[i] = array[j]
+            array[j] = temp
+        })
+        return array
+    }
 
-   colors.forEach((color, i) => {
-    const j = Math.floor(Math.random() * i)
-    const temp = colors[i]
-    colors[i] = colors[j]
-    colors[j] = temp
-  })
+    const doubleColors = colors.concat(...colors);
+
+    sortArray(doubleColors);
 
 
-  const allSmallSquares: Array<Square> = colors.map(c => ({id: v1(), color: c, hidden: true}))
+    const allSmallSquares: Array<Square> = doubleColors.map(c => ({id: v1(), color: c, hidden: true}))
 
-  return (
-      <div className="App">
-        {allSmallSquares.map(s => <div>{s.color}</div>)}
-      </div>
-  );
+    return (
+        <div className="App">
+
+            {allSmallSquares.map(s => <div key={s.id}>{s.color}</div>)}
+
+        </div>
+    );
 }
 
 export default App;
